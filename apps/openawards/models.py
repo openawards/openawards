88eac6f584afdb18af8adfbc65deca0581d969cc
Alfred <alfredgg@yahoo.es>
2018-10-03 11:40:35 +0200

    Add User model and improve signals syntax

--- apps/openawards/models.py
+++ apps/openawards/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-from django.dispatch import receiver
 from django.db.models.signals import pre_save
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as DjangoUser
 from apps.openawards.lib.utils import slugify_model
+from apps.openawards.exceptions import EnrollNotValidException, NotValidVoteException
+
+
+class User(DjangoUser):
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(args, kwargs)
+
+    def vote(self, work, award):
+        if work.author != self or Vote.objects.filter(award=award, work=work, fan=self).first() is not None:
+            raise NotValidVoteException
+        Vote(award=award, work=work, fan=self).save()
 
 
 class License(models.Model):
@@ -20,12 +30,11 @@ class Work(models.Model):
     description = models.TextField()
     cover = models.ImageField(null=True)
     created = models.DateField(null=True, blank=True)
-    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
-
-@receiver(pre_save, sender=Work)
-def work_pre_save(sender, instance, *args, **kwargs):
-    slugify_model(instance, 'title')
+    @classmethod
+    def pre_save(cls, sender, instance, **kwargs):
+        slugify_model(instance, 'title')
 
 
 class Award(models.Model):
@@ -35,6 +44,11 @@ class Award(models.Model):
     image = models.ImageField(null=True)
     active = models.BooleanField(default=False)
     description = models.TextField()
+    works = models.ManyToManyField(Work)
+
+    @classmethod
+    def pre_save(cls, sender, instance, **kwargs):
+        slugify_model(instance, 'name')
 
     def get_absolute_url(self):
         # return reverse('award-detail', args=[str(self.slug)])
@@ -43,13 +57,18 @@ class Award(models.Model):
     def __str__(self):
         return self.name
 
-
-@receiver(pre_save, sender=Award)
-def award_pre_save(sender, instance, *args, **kwargs):
-    slugify_model(instance, 'name')
+    def enroll_work(self, work):
+        if not self.active or work in self.works.all():
+            raise EnrollNotValidException()
+        self.works.add(work)
+        self.save()
 
 
 class Vote(models.Model):
     fan = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
     work = models.ForeignKey('Work', on_delete=models.CASCADE, null=False)
     award = models.ForeignKey('Award', on_delete=models.CASCADE, null=False)
+
+
+pre_save.connect(Award.pre_save, sender=Award)
+pre_save.connect(Work.pre_save, sender=Work)
