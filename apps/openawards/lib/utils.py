a94f3c159683c7033c433b2bceb6124d324e5b33
Alfred <alfredgg@yahoo.es>
2018-10-03 14:56:11 +0200

    Improve slugify of title. When it's an updating do not change slug field

--- apps/openawards/lib/utils.py
+++ apps/openawards/lib/utils.py
@@ -5,10 +5,12 @@ from django.utils.text import slugify
 
 
 def slugify_model(instance, attrib, slug_attrib='slug', _iter=0):
-    model = instance._meta.model
     value = getattr(instance, attrib)
     assert isinstance(value, str)
+    prev_value = getattr(instance, slug_attrib)
+    if prev_value.startswith(slugify(value)):
+        return
     _value = slugify(value) if _iter == 0 else slugify(f'{value}-{_iter}')
     params = {slug_attrib: _value}
-    obj_is_new = model.objects.filter(**params).first() is None
+    obj_is_new = instance._meta.model.objects.filter(**params).first() is None
     setattr(instance, slug_attrib, _value) if obj_is_new else slugify_model(instance, attrib, slug_attrib, _iter + 1)
