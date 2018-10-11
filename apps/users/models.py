0d15c8b52277a12be3280ccd040452f3bb9071be
Alfred <alfredgg@yahoo.es>
2018-10-11 11:50:54 +0200

    Update user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,8 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+# from django.db import models
 
 
 class BaseUser(AbstractUser):
-    bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
 
     def generate_token(self):
         pass
