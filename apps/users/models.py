67d66aa59a5c4de6b5e378250ca0b717656f909f
Alfred <alfredgg@yahoo.es>
2018-10-11 12:48:06 +0200

    Sign up users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.db import models
+from uuid import uuid4
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 
 class BaseUser(AbstractUser):
+    token = models.CharField(max_length=36, blank=False, unique=True, null=False)
 
-    def generate_token(self):
-        pass
+    @receiver(pre_save, sender='users.BaseUser')
+    def generate_token(sender, instance, **kwargs):
+        instance.token = str(uuid4()).replace('-', '')
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from apps.users.forms import SignUpForm
 from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
 
 
 def signup(request):
@@ -11,12 +12,13 @@ def signup(request):
             user.is_active = False
             user.save()
             subject = 'Activate Your MySite Account'
-            message = render_to_string('account_activation_email.html', {
+            message = render_to_string('emails/user_registration.html', {
+                'domain': get_current_site(request).domain,
                 'user': user,
-                'token': account_activation_token.make_token(user),
+                'token': user.token
             })
             user.email_user(subject, message)
-            return render(request, 'registration/user_created.html', {'email': form.cleaned_data.get('email')})
+            return render(request, 'registration/user_registered.html', {'email': form.cleaned_data.get('email')})
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
