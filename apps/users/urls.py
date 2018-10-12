105f1b54f0fd242c9d3837f8338fbc807273394b
Alfred <alfredgg@yahoo.es>
2018-10-12 12:12:05 +0200

    Generate password in Django way

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -8,6 +8,6 @@ from apps.users import views
 urlpatterns = [
     path('users/login/', views.UsersLoginView.as_view(), name='login'),
     path('users/signup', views.signup, name='signup'),
-    path('users/activate/<token>/', views.activate, name='users_activate'),
+    path('users/activate/<uuid>/<token>/', views.activate, name='users_activate'),
     path('users/', include('django.contrib.auth.urls')),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -6,10 +6,16 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth import login
 from urllib.parse import urljoin
 from django.contrib.auth.views import LoginView
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.utils.encoding import force_text
+from .tokens import AccountActivationTokenGenerator
 
 
 def get_activate_url(request, user):
-    url = reverse('users_activate', args=(user.token,))
+    _id = urlsafe_base64_encode(force_bytes(user.pk)).decode('utf-8')
+    token = AccountActivationTokenGenerator().make_token(user)
+    url = reverse('users_activate', args=(_id, token,))
     domain = get_current_site(request).domain
     protocol = 'https' \
         if request.is_secure and not (domain.startswith('127.0.0.1') or domain.startswith('localhost')) \
@@ -26,11 +32,10 @@ def signup(request):
             user = form.save()
             user.is_active = False
             user.save()
-            subject = 'Activate Your MySite Account'
+            subject = 'Activate Your Account'
             message = render_to_string('emails/user_registration.html', {
                 'url': get_activate_url(request, user),
-                'user': user,
-                'token': user.token
+                'user': user
             })
             user.email_user(subject, message)
             return render(request, 'registration/user_registered.html', {'email': form.cleaned_data.get('email')})
@@ -39,15 +44,15 @@ def signup(request):
     return render(request, 'registration/signup.html', {'form': form})
 
 
-def activate(request, token):
+def activate(request, uuid, token):
     try:
-        user = get_user_model().objects.get(token=token)
+        _id = force_text(urlsafe_base64_decode(uuid))
+        user = get_user_model().objects.get(pk=_id)
     except (TypeError, ValueError, OverflowError, get_user_model().DoesNotExist):
         user = None
 
-    if user is not None and not user.is_active:
+    if user is not None and not user.is_active and AccountActivationTokenGenerator().check_token(user, token):
         user.is_active = True
-        user.generate_token()
         user.save()
         login(request, user)
         # TODO: Add successful activation message
