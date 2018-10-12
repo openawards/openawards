2f1f6ef6ffca3123c10ff0753ed0feb4b6ea0596
Alfred <alfredgg@yahoo.es>
2018-10-12 10:28:49 +0200

    Check debug secret key is not the same than production secret key

--- OpenAwards/settings/dev.py
+++ OpenAwards/settings/dev.py
@@ -131,3 +131,5 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 AUTH_USER_MODEL = 'users.BaseUser'
+
+DEV_SETTINGS_MODULE = 'OpenAwards.settings.dev'
