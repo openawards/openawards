6f1c079acb5e933a3873d7b1c37eac0f556b5016
Alfred <alfredgg@yahoo.es>
2018-10-17 18:37:36 +0200

    Anonymous user avatar

--- apps/openawards/static/js/image-control.js
+++ apps/openawards/static/js/image-control.js
@@ -1,12 +1,15 @@
 "use strict;"
 
-function imageControl(getImgButton, fileField, profileImage) {
+function imageControl(getImgButton, fileField, profileImage, callback) {
     var fr = new FileReader();
 
     fileField.change(function (e) {
       var myFile = e.currentTarget.files[0];
       fr.addEventListener('load', function () {
         profileImage.attr('src', fr.result);
+        if (callback !== undefined) {
+            callback();
+        }
       });
       fr.readAsDataURL(myFile);
     });
