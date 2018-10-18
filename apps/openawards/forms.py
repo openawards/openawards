63e37a066331e2efddf0a5af9a547f2dd2cc3d8e
Alfred <alfredgg@yahoo.es>
2018-10-18 20:33:01 +0200

    Change user password on user account page

--- apps/openawards/forms.py
+++ apps/openawards/forms.py
@@ -4,6 +4,8 @@
 from django import forms
 from django.apps import apps
 from .lib.form_fields import ExtendedImageField
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import PasswordChangeForm
 
 
 class WorkForm(forms.ModelForm):
@@ -17,5 +19,38 @@ class UserAccountForm(forms.ModelForm):
         model = apps.get_model('openawards', 'User')
         fields = ('avatar', 'first_name', 'last_name')
 
-    avatar = ExtendedImageField()
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match."
+    }
 
+    avatar = ExtendedImageField(required=False)
+    new_password1 = forms.CharField(
+        label='New password',
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False
+    )
+    new_password2 = forms.CharField(
+        label='New password confirmation',
+        strip=False,
+        widget=forms.PasswordInput,
+        required=False
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        self.user = user
+        self.password_changed = False
+        super().__init__(*args, **kwargs)
+
+    def clean_new_password2(self):
+        if not self.cleaned_data.get('new_password1') and not self.cleaned_data.get('new_password2'):
+            return None
+        self.password_changed = True
+        PasswordChangeForm.clean_new_password2(self)
+
+    def save(self, commit=True):
+        if self.password_changed:
+            password = self.cleaned_data["new_password1"]
+            self.user.set_password(password)
+        return super().save(commit)
--- apps/openawards/views/AccountFormView.py
+++ apps/openawards/views/AccountFormView.py
@@ -5,6 +5,9 @@ from django.views import generic
 from django.apps import apps
 from apps.openawards.mixins import UserViewMixin
 from ..forms import UserAccountForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 
 
 class AccountFormView(UserViewMixin, generic.UpdateView):
@@ -13,13 +16,19 @@ class AccountFormView(UserViewMixin, generic.UpdateView):
     form_class = UserAccountForm
     success_url = 'account'
 
+    def get_form(self, form_class=None):
+        return self.form_class(self.request.user, **self.get_form_kwargs())
+
     def form_valid(self, form):
         # TODO: Remove the old avatar image from the store service
         # TODO: Change the image size
         # TODO: Change the image name for a random one
         user = form.save(commit=False)
         user.save()
-        return super().form_valid(form)
+        if form.password_changed:
+            login(self.request, user)
+            update_session_auth_hash(self.request, user)
+        return HttpResponseRedirect(self.get_success_url())
 
     def form_invalid(self, form):
         return super().form_invalid(form)
