1d5c23fb04713e113d11f87e831b38809f33744e
Pere <pere@perepicornell.com>
2018-10-19 11:45:17 +0200

    Almost finished, pending to fix the Vote() exception problem.

--- apps/openawards/management/commands/generateFakeData.py
+++ apps/openawards/management/commands/generateFakeData.py
@@ -23,21 +23,19 @@ class Command(BaseCommand):
         self.stdout.write(self.style.SUCCESS('Fake data for model %s created.' % 'License'))
 
         reg = Award(name="Animated shorts award",
-                    created=strTimeProp(prop=random.random()), active=bool(random.getrandbits(1)),
+                    created=strTimeProp(prop=random.random()), active=True,
                     description=random_string_generator(random.randint(50,500), True))
         reg.save()
-        reg = Award(name="Open Music Best Album", created=strTimeProp(prop=random.random()),
-                    active=bool(random.getrandbits(1)),
+        reg = Award(name="Open Music Best Album",
+                    created=strTimeProp(prop=random.random()), active=True,
                     description=random_string_generator(random.randint(50,500), True))
         reg.save()
         reg = Award(name="Fiction Novel Collective Culture Award",
-                    created=strTimeProp(prop=random.random()),
-                    active=bool(random.getrandbits(1)),
+                    created=strTimeProp(prop=random.random()), active=True,
                     description=random_string_generator(random.randint(50,500), True))
         reg.save()
         reg = Award(name="Essay and Academic Open Knowledge Award",
-                    created=strTimeProp(prop=random.random()),
-                    active=bool(random.getrandbits(1)),
+                    created=strTimeProp(prop=random.random()), active=True,
                     description=random_string_generator(random.randint(50,500), True))
         reg.save()
         self.stdout.write(self.style.SUCCESS('Fake data for model %s created.' % 'Awards'))
@@ -58,7 +56,34 @@ class Command(BaseCommand):
             reg.save()
         self.stdout.write(self.style.SUCCESS('Fake data for model %s created.' % 'Works'))
 
-        #TODO: Loop through Users to .vote() works
+        for award in Award.objects.order_by("id").all():
+            if award.id == 1:
+                for x in range(1, 12):
+                    award.enroll_work(work=x)
+            elif award.id == 2:
+                for x in range(13, 24):
+                    award.enroll_work(work=x)
+            elif award.id == 3:
+                for x in range(25, 36):
+                    award.enroll_work(work=x)
+            elif award.id == 4:
+                for x in range(37, 50):
+                    award.enroll_work(work=x)
+        self.stdout.write(self.style.SUCCESS('Works enrolled to corresponding awards.'))
+
+        for user in User.objects.order_by("id").all():
+            # Randomly deciding how many votes this user will cast
+            for x in range(random.randint(5,30)):
+                # Going through all 50 works…
+                for w in range(1,50):
+                    # …and randomly making this user vote to 25% of them.
+                    if random.randint(0,3) == 3:
+                        #TODO: Trying to loop through all the Awards that this Work is enrolled at, but failing!
+                        work = Work.objects.get(id=w)
+                        for enrolled_award in work.award_set.all():
+                            user.vote(work=work, award=enrolled_award)
+        self.stdout.write(self.style.SUCCESS('All users voted to random works, in every award this work is enrolled.'))
+
 
 def random_string_generator(size=10, with_space=False, chars=string.ascii_lowercase + string.digits):
     if with_space:
