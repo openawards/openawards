48c8fd03901d6930c4aff17e94dc3ee0cff989ad
Alfred <alfredgg@yahoo.es>
2018-10-20 12:31:28 +0200

    Home first version

--- apps/openawards/views/HomeView.py
+++ apps/openawards/views/HomeView.py
@@ -11,5 +11,5 @@ class HomeView(generic.base.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['awards'] = apps.get_model('openawards', 'Award').objects.all()
-        context['works'] = apps.get_model('openawards', 'Work').objects.all()
+        context['works'] = apps.get_model('openawards', 'Work').objects.order_by('?').all()[:5]
         return context
