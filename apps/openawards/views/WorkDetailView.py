490685e30a0c83373de459730dfae1b7537e1a6d
Alfred <alfredgg@yahoo.es>
2018-10-31 20:05:20 +0100

    Allow vote in work page

--- apps/openawards/views/WorkDetailView.py
+++ apps/openawards/views/WorkDetailView.py
@@ -3,7 +3,16 @@
 
 from django.views import generic
 from django.apps import apps
+from openawards.models import Award
 
 
 class WorkDetailView(generic.DetailView):
     model = apps.get_model('openawards', 'Work')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.object in self.request.user.works.all():
+            context['forbidden_awards'] = Award.objects.all()
+        else:
+            context['forbidden_awards'] = [vote.award for vote in self.request.user.votes.filter(work=self.object)]
+        return context
