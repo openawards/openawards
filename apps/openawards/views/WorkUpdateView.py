b8431cb2ce3382d6575e4493adb504266df1241c
Alfred <alfredgg@yahoo.es>
2018-11-01 13:11:53 +0100

    Allow work editing

---
+++ apps/openawards/views/WorkUpdateView.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.views import generic
+from django.apps import apps
+from ..forms import WorkForm
+from django.shortcuts import reverse
+from django.http import HttpResponse
+
+
+class WorkUpdateView(generic.UpdateView):
+    model = apps.get_model('openawards', 'Work')
+    form_class = WorkForm
+    template_name = 'openawards/work.html'
+
+    def get_form(self, form_class=None):
+        return self.form_class(self.request.user, **self.get_form_kwargs())
+
+    def get(self, request, *args, **kwargs):
+        current_work = self.get_object()
+        if current_work.creator != self.request.user:
+            return HttpResponse(status=500)
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        current_work = self.get_object()
+        if current_work.creator != self.request.user:
+            return HttpResponse(status=500)
+        self.success_url = reverse('work', args=[current_work.slug])
+        return super().post(request, *args, **kwargs)
