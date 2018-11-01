1984ca149b1fa031bdd85d28ad58f5b4ae8cbbc0
Alfred <alfredgg@yahoo.es>
2018-11-01 14:46:51 +0100

    Manage past awards

--- apps/openawards/views/PastAwardsListView.py
+++ apps/openawards/views/PastAwardsListView.py
@@ -3,7 +3,9 @@
 
 from django.views import generic
 from django.apps import apps
+from django.utils import timezone
 
 
 class PastAwardsListView(generic.ListView):
     model = apps.get_model('openawards', 'Award')
+    queryset = apps.get_model('openawards', 'Award').objects.filter(ends_on__lte=timezone.now())
